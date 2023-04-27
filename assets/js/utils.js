e489b760dae856f4d10c3c0dbac958e873366be7
Manuela <manuelarodriguezdavila@outlook.com>
2023-04-27 07:13:32 -0500

    commit-3

--- assets/js/utils.js
+++ assets/js/utils.js
@@ -7,5 +7,22 @@ export const traerTienda = async () => {
 // export class producto
 
 export class producto {
-    
+    imagen = ""
+    title = ""
+
+    constructor(imagen, title) {
+        this.imagen = image;
+        this.title = title;
+    }
+
+    render() {
+        const div = document.createElement ('div')
+        const jpg = document.createElement ('img')
+        const titulo = document.createElement ('p')
+        jpg.src = this.imagen;
+
+        div.appendChild(jpg);
+        div.appendChild(titulo);
+        return div;
+    }
 }
